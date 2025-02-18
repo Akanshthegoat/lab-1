b671ef497fe13268e915a859ca98c3b4cd2e03ad
Akanshthegoat <akanshkohli002@gmail.com>
2025-02-18 14:29:24 +0530

    Add files via upload

---
+++ equality.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+if num1==num2:
+    print(f"\nThe equal number is: {num1}")
+elif num1==num3:
+    print(f"\nThe equal number is: {num1}")
+elif num2==num3:
+    print(f"\nThe equal number is: {num2}")
+else:
+    print(f"\n no numbers are equal")
---
+++ last.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter your number: "))
+square1=num1**2
+cube1=num1**3
+if num1%2==0:
+    print(f"\nYour square is:{square1}")
+else:
+    print(f"\nYour cube is:{cube1}")
